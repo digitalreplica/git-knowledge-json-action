52074eca4056da2dfa657c8a8d48feaf557496d1
digitalreplica-llc <64011376+digitalreplica-llc@users.noreply.github.com>
2021-10-29 18:06:15 -0400

    test listing branches

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
+async function run() {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('myToken');
+
+    const octokit = github.getOctokit(myToken)
+
+    const branches = await octokit.rest.repos.listBranches({
+      'digitalreplica',
+      'hacking',
+    });
+
+    console.log(branches);
+}
+
+run();
+
+
+//try {
   // `who-to-greet` input defined in action metadata file
   //const nameToGreet = core.getInput('who-to-greet');
   //console.log(`Hello ${nameToGreet}!`);
@@ -10,22 +30,22 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
-  const fs = require('fs')
+//  const fs = require('fs')
 
-  fs.readFile('repos.txt', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
+//  fs.readFile('repos.txt', 'utf8' , (err, data) => {
+//    if (err) {
+//      console.error(err)
+//      return
+//    }
     // split the contents by new line
-    const lines = data.split(/\r?\n/);
+//    const lines = data.split(/\r?\n/);
 
-    for (let repo of lines) {
-      if (repo.length == 0) { continue; }
-      console.log(repo.length)
-    }
-  })
+//    for (let repo of lines) {
+//      if (repo.length == 0) { continue; }
+//      console.log(repo.length)
+//    }
+//  })
 
-} catch (error) {
-  core.setFailed(error.message);
-}
+//} catch (error) {
+//  core.setFailed(error.message);
+//}
